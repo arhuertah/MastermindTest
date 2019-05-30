c97f1ae09eab1ce28a984b0d8dd00f16e9959c98
Armando Huerta Hoffmann <ahuertahoffmann@live.com.mx>
2019-05-30 00:44:14 -0500

    Initial Commit

---
+++ MastermindTest/Mastermind.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastermindTest
+{
+    public class Mastermind
+    {
+        /// <summary>
+        /// Correct Answer
+        /// </summary>
+        public int[] Answer { get; set; }
+
+        /// <summary>
+        /// Number of attempts
+        /// </summary>
+        public int Attempts { get; set; }
+
+        /// <summary>
+        /// Winner flag
+        /// </summary>
+        public bool Won { get; set; }
+
+        public int NumberOfMinus { get; set; }
+
+        public int NumberOfAccerts { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Mastermind()
+        {
+            Attempts = 0;
+            Answer = new int[4];
+        }
+
+        /// <summary>
+        /// Generates randomly the correct answer
+        /// </summary>
+        /// <returns></returns>
+        public string GenerateAnswer()
+        {
+            var random = new Random();
+
+            //Assign each element with random number
+            for (int i = 0; i < Answer.Length; i++)
+            {
+                Answer[i] = random.Next(1, 6);
+            }
+
+            //Convert int Array to String
+            var returnAnswer = String.Join("", Answer.Select(x => x.ToString()).ToArray());
+
+            return returnAnswer;
+        }
+
+        /// <summary>
+        /// Validates User digits submited
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <returns></returns>
+        public bool CheckUserInput(string userInput)
+        {
+            //Validate if userInput is a number
+            int number = 0;
+            var isOk = int.TryParse(userInput, out number);
+
+            //Validates userInput length
+            isOk = userInput.Length == 4 ? true : false;
+
+            //Converts the string into int array for validations
+            var tryAnswer = userInput.ToCharArray().Select(s => int.Parse(s.ToString()));
+
+            //validates answer boundaries
+            foreach (int digit in tryAnswer)
+            {
+                if (digit <= 0 || digit > 6)
+                {
+                    isOk = false;
+                }
+            }
+
+            return isOk;            
+        }
+
+        /// <summary>
+        /// Assuming string is validated we will check the result from userInput
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <returns></returns>
+        public bool Guessed(string userInput)
+        {
+            this.NumberOfAccerts = 0;
+            this.NumberOfMinus = 0;
+            var tryAnswer = userInput.ToCharArray().Select(s => int.Parse(s.ToString())).ToArray();
+
+            for (int index = 0; index < tryAnswer.Count(); index++)
+            {
+                if (Answer.Contains(tryAnswer[index]))
+                {
+                    this.NumberOfMinus++;
+                }
+
+                if (Answer[index] == tryAnswer[index])
+                {
+                    this.NumberOfAccerts++;
+                    this.NumberOfMinus--;
+                }
+            }
+
+            if (this.NumberOfAccerts == 4)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ MastermindTest/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastermindTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mastermind Test");
+            Console.WriteLine("\nInstructions: Enter 4 digits");
+            Console.WriteLine("\nA minus (-) sign will be displayed for every digit that is correct " +
+                "\nbut in the wrong position, and a plus(+) sign should be printed for every digit that " +
+                "\nis both correct and in the correct position.\n");
+
+            var masterMindGame = new Mastermind();
+
+            //Generate an answer and save at console to be displayed at the end
+            var answer = masterMindGame.GenerateAnswer();
+
+            //For testing purposes
+            Console.WriteLine($"Correct Answer {answer}");
+
+            do
+            {
+                Console.WriteLine($"Introduce your 4 digits for attempt No." +
+                    $"{masterMindGame.Attempts + 1} and click Enter");
+
+                var userInput = Console.ReadLine();
+
+                if (masterMindGame.CheckUserInput(userInput))
+                {                    
+                    masterMindGame.Attempts++;
+
+                    if (masterMindGame.Guessed(userInput))
+                    {
+                        masterMindGame.Won = true;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(new string('+', masterMindGame.NumberOfAccerts));
+                        Console.WriteLine(new string('-', masterMindGame.NumberOfMinus));                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please introduce only numbers from 1 to 6");
+                }
+
+            } while (masterMindGame.Attempts < 10);
+
+            if (masterMindGame.Won)
+            {
+                Console.WriteLine("Congratulations!!! You are Mastermind Winner!");
+            }
+            else
+            {
+                Console.WriteLine($"You lost! the correct answer is: {answer}");
+            }
+
+            Console.Read();
+        }
+    }
+}
